0e79e962ead924f12161a40a543c18ca59c5cbb2
unknown <joaquinfluna4@gmail.com>
2022-03-10 19:55:24 -0300

    Se limpia el codigo, se cambia a css grid

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
 				console.log("este es B", numberB);
 				console.log("este es A despues de", numberA);
 			}
-		} //esta logica no esta bien
+		} //esta logica no esta bien n
 
 		if (operator === "AC") {
 			setDisplay("");
@@ -36,124 +36,129 @@ function App() {
 		setDisplay(display + e.target.value);
 	};
 	return (
-		<div className="container">
-			<div name="display">
-				<h2> This is a result: {result} </h2>
-				<h2> This is a operation: {display} </h2>
+		<div className="container-calc">
+			<div name="display" className="container-display">
+				<h2 className="display"> This is a result: {result} </h2>
+				<h2 className="display"> This is a operation: {display} </h2>
 			</div>
-			<table>
-				<tbody>
-					<tr name="FUNCIONES">
-						<td>
-							<button name="AC" onClick={handleOperator} id="operator">
-								AC
-							</button>
-						</td>
-						<td>
-							<button name="delete" onClick={handleOperator} id="operator">
-								DEL
-							</button>
-						</td>
-						<td>
-							<button name="percentage" onClick={handleOperator} id="operator">
-								%
-							</button>
-						</td>
-						<td>
-							<button name="division" onClick={handleOperator} id="operator">
-								/
-							</button>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<button value="7" onClick={handleNumber}>
-								7
-							</button>
-						</td>
-						<td>
-							<button value="8" onClick={handleNumber}>
-								8
-							</button>
-						</td>
-						<td>
-							<button value="9" onClick={handleNumber}>
-								9
-							</button>
-						</td>
-						<td>
-							<button
-								name="multiplication"
-								onClick={handleOperator}
-								id="operator"
-							>
-								X
-							</button>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<button value="4" onClick={handleNumber}>
-								4
-							</button>
-						</td>
-						<td>
-							<button value="5" onClick={handleNumber}>
-								5
-							</button>
-						</td>
-						<td>
-							<button value="6" onClick={handleNumber}>
-								6
-							</button>
-						</td>
-						<td>
-							<button name="minus" onClick={handleOperator} id="operator">
-								-
-							</button>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<button value="1" onClick={handleNumber}>
-								1
-							</button>
-						</td>
-						<td>
-							<button value="2" onClick={handleNumber}>
-								2
-							</button>
-						</td>
-						<td>
-							<button value="3" onClick={handleNumber}>
-								3
-							</button>
-						</td>
-						<td>
-							<button name="plus" onClick={handleOperator} id="operator">
-								+
-							</button>
-						</td>
-					</tr>
-					<tr>
-						<td>
-							<button value="0" onClick={handleNumber}>
-								0
-							</button>
-						</td>
-						<td>
-							<button name="dot" onClick={handleOperator}>
-								.
-							</button>
-						</td>
-						<td>
-							<button name="equal" onClick={handleOperator} id="operator">
-								=
-							</button>
-						</td>
-					</tr>
-				</tbody>
-			</table>
+
+			<div name="container-keys" className="container-keys">
+				<button
+					name="AC"
+					onClick={handleOperator}
+					id="operator"
+					className="operator"
+				>
+					AC
+				</button>
+
+				<button
+					name="delete"
+					onClick={handleOperator}
+					id="operator"
+					className="operator"
+				>
+					DEL
+				</button>
+
+				<button
+					name="percentage"
+					onClick={handleOperator}
+					id="operator"
+					className="operator"
+				>
+					%
+				</button>
+
+				<button
+					name="division"
+					onClick={handleOperator}
+					id="operator"
+					className="operator"
+				>
+					/
+				</button>
+
+				<button value="7" onClick={handleNumber} className="numbers">
+					7
+				</button>
+
+				<button value="8" onClick={handleNumber} className="numbers">
+					8
+				</button>
+
+				<button value="9" onClick={handleNumber} className="numbers">
+					9
+				</button>
+
+				<button
+					name="multiplication"
+					onClick={handleOperator}
+					id="operator"
+					className="operator"
+				>
+					X
+				</button>
+
+				<button value="4" onClick={handleNumber} className="numbers">
+					4
+				</button>
+
+				<button value="5" onClick={handleNumber} className="numbers">
+					5
+				</button>
+
+				<button value="6" onClick={handleNumber} className="numbers">
+					6
+				</button>
+
+				<button
+					name="minus"
+					onClick={handleOperator}
+					id="operator"
+					className="operator"
+				>
+					-
+				</button>
+
+				<button value="1" onClick={handleNumber} className="numbers">
+					1
+				</button>
+
+				<button value="2" onClick={handleNumber} className="numbers">
+					2
+				</button>
+
+				<button value="3" onClick={handleNumber} className="numbers">
+					3
+				</button>
+
+				<button
+					name="plus"
+					onClick={handleOperator}
+					id="operator"
+					className="operator"
+				>
+					+
+				</button>
+
+				<button value="0" onClick={handleNumber} className="numbers">
+					0
+				</button>
+
+				<button name="dot" onClick={handleOperator} className="numbers">
+					.
+				</button>
+
+				<button
+					name="equal"
+					onClick={handleOperator}
+					id="operator"
+					className="operator"
+				>
+					=
+				</button>
+			</div>
 		</div>
 	);
 }
