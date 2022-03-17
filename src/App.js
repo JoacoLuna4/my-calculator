753e30a0c221ec1f938623079b54d8dc2539882d
unknown <joaquinfluna4@gmail.com>
2022-03-17 16:37:37 -0300

    Logica de operaciones y estilos :)

--- src/App.js
+++ src/App.js
@@ -1,63 +1,78 @@
 import "./index.css";
 import React, { useState } from "react";
 function App() {
-	const [display, setDisplay] = useState("");
 	const [result, setResult] = useState("");
 	const [numberA, setNumberA] = useState("");
 	const [numberB, setNumberB] = useState("");
 	const [currentOperator, setcurrentOperator] = useState("");
 
-	const handleOperator = (e) => {
-		setcurrentOperator(e);
-		console.log(currentOperator + " current operator ");
-		// if (currentOperator === "AC") {
-		// 	setDisplay("");
-		// } else if (currentOperator === "delete") {
-		// 	setDisplay(display.substring(0, display.length - 1));
-		// }
-	};
-
-	// const handleNumber = (e) => {
-	// 	setDisplay(display + e.target.value);
-	// };
 	const handleNumber = (e) => {
 		if (currentOperator === "") {
-			setNumberA(numberA + e.target.value);
+			setNumberA(numberA + e);
+
 			console.log("ESTE ES EL NUMBER A " + numberA);
 		} else {
-			setNumberB(numberB + e.target.value);
+			setNumberB(numberB + e);
 			console.log("este es el number B " + numberB);
+		}
+	};
+	const handleOperator = (e) => {
+		setcurrentOperator(e);
+		console.log(e);
+	};
+	function getResult() {
+		switch (currentOperator) {
+			case "+":
+				setResult(Number(numberA) + Number(numberB));
+
+				break;
+			case "-":
+				setResult(Number(numberA) - Number(numberB));
+				break;
+			case "*":
+				setResult(Number(numberA) * Number(numberB));
+				break;
+			case "/":
+				setResult(Number(numberA) / Number(numberB));
+				break;
+			case "%":
+				setResult(Number(numberA) % Number(numberB));
+				break;
+		}
+	}
+	const allClear = () => {
+		setNumberA("");
+		setNumberB("");
+		setResult("");
+		setcurrentOperator("");
+	};
+	const delNumber = () => {
+		if (currentOperator === "") {
+			setNumberA(numberA.substring(0, numberA.length - 1));
+		} else {
+			setNumberB(numberB.substring(0, numberB.length - 1));
 		}
 	};
 
 	return (
 		<div className="container-calc">
 			<div name="display" className="container-display">
-				<h2 className="display">
-					{" "}
-					This is a result: {!currentOperator
-						? numberA + currentOperator
-						: ""}{" "}
+				<h2 className="result">
+					{result ? result : !currentOperator ? numberA : numberB}
 				</h2>
-				<h2 className="display">
-					{" "}
-					This is a operation: {currentOperator ? numberA : numberB}{" "}
+				<h2 className="operation">
+					{currentOperator ? numberA + currentOperator : ""}
 				</h2>
 			</div>
 
 			<div name="container-keys" className="container-keys">
-				<button
-					name="AC"
-					onClick={handleOperator("AC")}
-					id="operator"
-					className="operator"
-				>
+				<button name="AC" onClick={allClear} id="operator" className="operator">
 					AC
 				</button>
 
 				<button
 					name="delete"
-					onClick={handleOperator("del")}
+					onClick={delNumber}
 					id="operator"
 					className="operator"
 				>
@@ -66,7 +81,7 @@ function App() {
 
 				<button
 					name="percentage"
-					onClick={handleOperator("%")}
+					onClick={() => handleOperator("%")}
 					id="operator"
 					className="operator"
 				>
@@ -75,87 +90,91 @@ function App() {
 
 				<button
 					name="division"
-					onClick={handleOperator("/")}
+					onClick={() => handleOperator("/")}
 					id="operator"
 					className="operator"
 				>
 					/
 				</button>
 
-				<button value="7" onClick={handleNumber} className="numbers">
+				<button value="7" onClick={() => handleNumber(7)} className="numbers">
 					7
 				</button>
 
-				<button value="8" onClick={handleNumber} className="numbers">
+				<button value="8" onClick={() => handleNumber(8)} className="numbers">
 					8
 				</button>
 
-				<button value="9" onClick={handleNumber} className="numbers">
+				<button value="9" onClick={() => handleNumber(9)} className="numbers">
 					9
 				</button>
 
 				<button
 					name="multiplication"
-					onClick={handleOperator("*")}
+					onClick={() => handleOperator("*")}
 					id="operator"
 					className="operator"
 				>
 					X
 				</button>
 
-				<button value="4" onClick={handleNumber} className="numbers">
+				<button value="4" onClick={() => handleNumber(4)} className="numbers">
 					4
 				</button>
 
-				<button value="5" onClick={handleNumber} className="numbers">
+				<button value="5" onClick={() => handleNumber(5)} className="numbers">
 					5
 				</button>
 
-				<button value="6" onClick={handleNumber} className="numbers">
+				<button value="6" onClick={() => handleNumber(6)} className="numbers">
 					6
 				</button>
 
 				<button
 					name="minus"
-					onClick={handleOperator("-")}
+					onClick={() => handleOperator("-")}
 					id="operator"
 					className="operator"
 				>
 					-
 				</button>
 
-				<button value="1" onClick={handleNumber} className="numbers">
+				<button onClick={() => handleNumber(1)} className="numbers">
 					1
 				</button>
 
-				<button value="2" onClick={handleNumber} className="numbers">
+				<button onClick={() => handleNumber(2)} className="numbers">
 					2
 				</button>
 
-				<button value="3" onClick={handleNumber} className="numbers">
+				<button value="3" onClick={() => handleNumber(3)} className="numbers">
 					3
 				</button>
 
 				<button
 					name="plus"
-					onClick={handleOperator}
+					onClick={() => handleOperator("+")}
 					id="operator"
 					className="operator"
 				>
 					+
 				</button>
 
-				<button value="0" onClick={handleNumber} className="numbers">
+				<button value="0" onClick={() => handleNumber(0)} className="numbers">
 					0
 				</button>
 
-				<button name="dot" onClick={handleOperator(".")} className="numbers">
+				<button
+					name="dot"
+					onClick={() => handleOperator(".")}
+					className="numbers"
+				>
 					.
 				</button>
 
 				<button
 					name="equal"
-					onClick={handleOperator}
+					onClick={getResult}
 					id="operator"
 					className="operator"
 				>
@@ -167,3 +186,10 @@ function App() {
 }
 
 export default App;
+function suma(a, b) {
+	return a + b;
+}
+function resta(a, b) {
+	return a - b;
+}
+suma(resta(2, 1), suma(2, 2));
